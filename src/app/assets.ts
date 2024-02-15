af0d49f748315ec0cdb57610ee735ed19c93135e
Nikita-Pet <nikita_petrishchev94@mail.ru>
2024-02-15 04:46:57 +0300

    V11-12-02

--- src/app/assets.ts
+++ src/app/assets.ts
@@ -1,11 +1,13 @@
 export const availableTypes = {
-    mistake: 'Ошибка',
     remark: 'Замечание',
+    mistake: 'Ошибка',
     recommendation: 'Рекомендация',
 }
+export const typesOrder = ['remark', 'mistake', 'recommendation']
 
 export const availableStatuses = {
     waiting: 'В очереди',
     processing: 'В работе',
     ready: 'Готово',
 }
+export const statusesOrder = ['waiting', 'processing', 'ready']
--- src/app/funcs/sortList.ts
+++ src/app/funcs/sortList.ts
@@ -1,19 +1,31 @@
-export default function sortList(list, sort) {
+import { typesOrder, statusesOrder } from 'app/assets'
+
+export default function sortList(list: any[], sort: string) {
     const sortedList = [...list]
     const availableSorts = ['date', 'autor', 'description', 'type', 'status']
 
     if (availableSorts.includes(sort)) {
         sortedList.sort(function (obj1, obj2) {
-            let objProp1 = obj1[sort]
-            let objProp2 = obj2[sort]
+            let prop1 = obj1[sort]
+            let prop2 = obj2[sort]
 
             if (sort === 'date') {
-                objProp1 = new Date(objProp1)
-                objProp2 = new Date(objProp2)
+                prop1 = new Date(prop1)
+                prop2 = new Date(prop2)
             }
 
-            if (objProp1 > objProp2) return 1
-            else if (objProp1 < objProp2) return -1
+            if (sort === 'type') {
+                prop1 = typesOrder.indexOf(prop1)
+                prop2 = typesOrder.indexOf(prop2)
+            }
+
+            if (sort === 'status') {
+                prop1 = statusesOrder.indexOf(prop1)
+                prop2 = statusesOrder.indexOf(prop2)
+            }
+
+            if (prop1 > prop2) return 1
+            else if (prop1 < prop2) return -1
             else return 0
         })
     }
